cfe9f60c2917bbbeb3a50f4fc87fa8e50a37144a
AniaP6 <apieszka@student.agh.edu.pl>
2026-05-18 17:13:24 +0200

    Final

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/lab5/', 
+  plugins: [
+    tailwindcss(),
+  ],
+  base: '/lab5/',
 })
